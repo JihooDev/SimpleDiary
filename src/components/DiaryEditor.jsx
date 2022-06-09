e3812dbff14e426ac7d64652c6a35edaa0c888bb
Jihoo <wlgn829@kakao.com>
2022-06-09 09:14:16 +0900

    Detail 구현

--- src/components/DiaryEditor.jsx
+++ src/components/DiaryEditor.jsx
@@ -5,38 +5,8 @@ import MyButton from './../components/MyButton';
 import MyHeader from './../components/MyHeader';
 import DiaryList from './DiaryList';
 import EmotionItem from './EmotionItem';
-
-const emotionList = [
-	{
-		emotion_id: 1,
-		emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-		emotion_descript: '완전 좋음',
-	},
-	{
-		emotion_id: 2,
-		emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-		emotion_descript: '좋음',
-	},
-	{
-		emotion_id: 3,
-		emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-		emotion_descript: '그럭저럭',
-	},
-	{
-		emotion_id: 4,
-		emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-		emotion_descript: '나쁨',
-	},
-	{
-		emotion_id: 5,
-		emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-		emotion_descript: '욕나와',
-	},
-];
-
-const getStringDate = date => {
-	return date.toISOString().slice(0, 10);
-};
+import { getStringDate } from '../util/date';
+import { emotionList } from '../util/emotion_list';
 
 export default function DiaryEditor({ isEdit, originData }) {
 	const contentRef = useRef();
